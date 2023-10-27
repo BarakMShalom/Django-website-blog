51f6ae924ba3a45d62c2bf55270704ecb6f7932d
Barak Shalom <118911734+BarakMShalom@users.noreply.github.com>
2023-10-27 20:53:54 +0200

    Add post list and post update logic

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .models import Post
-
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+)
 
 def blog_home(request):
     """
@@ -19,6 +23,27 @@ def blog_home(request):
         "posts": Post.objects.all()
     }
     return render(request, "blog/home.html", context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/home.html"
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
 
 
 def blog_about(request):
